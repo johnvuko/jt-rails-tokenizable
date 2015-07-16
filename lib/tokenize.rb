772ab9521f95d8ef604c0f7b3d92d95d6df464be
Jonathan Tribouharet <jonathan@siu-soon.com>
2015-07-17 01:14:44 +0200

    Bug fix

--- lib/tokenize.rb
+++ lib/tokenize.rb
@@ -5,30 +5,30 @@ module JT::Rails::Tokenizable::Tokenize
 		before_validation :jt_rails_generate_tokens, on: :create
 	end
 
-	module ClassMethods
+	class_methods do
 
 		def tokenize(field, options = {})
-			@@jt_rails_token_fields ||= {}
-
-			@@jt_rails_token_fields[field.to_sym] = options
+			jt_rails_token_fields[field.to_sym] = options
 
 			if options.fetch(:valiations, true)
 				validates field, presence: true, uniqueness: true
 			end
 		end
 
+		def jt_rails_token_fields
+			@jt_rails_token_fields ||= {}
+		end
+
 	end
 
 	def jt_rails_generate_tokens
-		@@jt_rails_token_fields ||= {}
-
-		for field in @@jt_rails_token_fields.keys
+		for field in self.class.jt_rails_token_fields.keys
 			generate_new_token(field)
 		end
 	end
 
 	def generate_new_token(field)
-		size = @@jt_rails_token_fields[field.to_sym].fetch(:size, 32)
+		size = self.class.jt_rails_token_fields[field.to_sym].fetch(:size, 32)
 
 		self[field.to_sym] = loop do
 			random_token = SecureRandom.hex(size)
